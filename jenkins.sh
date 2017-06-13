d165e36d3f56ca315204a3247e907449d7cf9235
crumpstrr33 <jscott12009@gmail.com>
2017-06-13 14:06:37 -0700

    Change method to obtain rhel version. Change implementation for different directory

--- jenkins.sh
+++ jenkins.sh
@@ -1,58 +1,46 @@
 #!/bin/bash
 
-# Use names psbuild-rhel5/6/7
-FULL_HOSTNAME=$(hostname)
-HOSTNAME=(${FULL_HOSTNAME//./ }) && HOSTNAME=${HOSTNAME[0]}
+RHEL_VER=UNKNOWN
+cat /etc/redhat-release | grep -q "release 7" && RHEL_VER=7
+cat /etc/redhat-release | grep -q "release 6" && RHEL_VER=6
+cat /etc/redhat-release | grep -q "release 5" && RHEL_VER=5
 
-# Another way:
-# if [ $(grep -o '-' <<< $HOST1 | wc -l) -eq 2 ]; then
-# If extra tacked on (like -01/-02 on rhel7, remove it)
-if [ $(echo $HOSTNAME | tr -cd '-' | wc -c) -eq 2 ]; then
-	HOSTNAME=${HOSTNAME%-*}
-fi
-echo "$PREFIX Using name $HOSTNAME for $FULL_HOSTNAME..."
-
-UNZIP=false
-PREFIX="[JENKINS SCRIPT]:"
 conda_setup="/reg/g/psdm/bin/conda_setup"
+PREFIX="[JENKINS SCRIPT]:"
+BUILDER=$(whoami)
+HOSTNAME=$(hostname)
+BASE_DIR="/reg/g/psdm/sw/conda/inst/miniconda2-prod-rhel${RHEL_VER}/envs"
 VERSION="9.9.9"
-BASE_DIR=$1
-CONDA_DIR="$BASE_DIR/conda-root"
+UNZIP=true
 MAX_BUILDS=10
+
 
 # Exit as failure if any errors so Jenkins sees it as a failure
 set -e
 
-cd $BASE_DIR
-source $conda_setup ""
 
-BUILDER=$(whoami)
-echo "$PREFIX Building on ${FULL_HOSTNAME} as ${BUILDER}..."
+# Source conda and begin script
+source $conda_setup ""
+echo "$PREFIX Building on ${HOSTNAME} as ${BUILDER}..."
+
 
 # Initial setup, clean directory before using
-[ ! -d "${HOSTNAME}_nightly" ] && mkdir ${HOSTNAME}_nightly
-[ -d "tmp_${HOSTNAME}_nightly" ] && rm -rf tmp_${HOSTNAME}_nightly
-[ -d "conda-root" ] && rm -rf conda-root
-[ -d "conda-bld" ] && rm -rf conda-bld
-
-# Makes directories
-echo "$PREFIX Creating dirs..."
-mkdir -p conda-root/downloads/anarel
-
-
-# cd into directories
-cd conda-root
+cd $BASE_DIR
+[ -d "tmp_nightly" ] && rm -rf tmp_nightly
+mkdir -p tmp_nightly/conda-root/downloads/anarel
+TMP_DIR="$BASE_DIR/tmp_nightly"
+CONDA_DIR="$TMP_DIR/conda-root"
+cd $TMP_DIR
 
 
 # Remove old tags and get new ones
-[ ! -z $(ls downloads/anarel) ] && rm downloads/anarel/*
+cd conda-root
 echo "PREFIX Retrieving tags..."
 ana-rel-admin --force --cmd psana-conda-src --name $VERSION --basedir $CONDA_DIR --tagsfile /reg/g/psdm/sw/conda/manage/config/psana-conda-svn-pkgs
 
 
 # Get recipes and edit meta.yaml
 echo "$PREFIX Retrieving recipe..."
-rm -rf psana-conda-opt
 cp -r /reg/g/psdm/sw/conda/manage/recipes/psana/psana-conda-opt .
 
 echo "$PREFIX Editing meta.yaml..."
@@ -61,46 +49,44 @@ sed -i "/source:/!b;n;c \ \ fn: $CONDA_DIR/downloads/anarel/{{ pkg }}-{{ version
 
 
 # Build it
-cd $BASE_DIR
-mkdir tmp_${HOSTNAME}_nightly
-
-cd conda-root
 echo "$PREFIX Building..."
-conda-build --output-folder $BASE_DIR/tmp_${HOSTNAME}_nightly psana-conda-opt
+conda-build --output-folder $TMP_DIR psana-conda-opt
 
 
 # Extracting or moving build
+echo "$PREFIX Done building..."
 cd $BASE_DIR
 DATE=`date +%Y%m%d_hour%H`
-mkdir ${HOSTNAME}_nightly/$DATE
-BUILD_FILE=$(ls tmp_${HOSTNAME}_nightly/linux-64 | grep psana-conda-$VERSION)
+mkdir ana-nightly-$DATE
+BUILD_FILE=$(ls $TMP_DIR/linux-64 | grep psana-conda-$VERSION)
 if [ $UNZIP = true ]; then
-	echo "$PREFIX Extracting build data to $BASE_DIR/${HOSTNAME}_nightly/$DATE..."
-	tar jxf tmp_${HOSTNAME}_nightly/linux-64/$BUILD_FILE -C ${HOSTNAME}_nightly/$DATE
+	echo "$PREFIX Extracting build data to $BASE_DIR/ana-nightly-$DATE..."
+	tar jxf $TMP_DIR/linux-64/$BUILD_FILE -C ana-nightly-$DATE
 else
-	echo "$PREFIX Moving .tar.bz2 file to $BASE_DIR/${HOSTNAME}_nightly/$DATE..."
-	mv tmp_${HOSTNAME}_nightly/linux-64/$BUILD_FILE ${HOSTNAME}_nightly/$DATE
+	echo "$PREFIX Moving .tar.bz2 file to $BASE_DIR/ana-nightly-$DATE..."
+	mv $TMP_DIR/linux-64/$BUILD_FILE ana-nightly-$DATE
 fi
 
+
 # Remove conda-bld extra directories
-echo "$PREFIX Removing conda-bld, conda-root and tmp_${HOSTNAME}_nightly directory from $BASE_DIR..."
+echo "$PREFIX Removing tmp_nightly directory from $BASE_DIR..."
 conda build purge
-rm -rf conda-bld conda-root tmp_${HOSTNAME}_nightly
+rm -rf tmp_nightly
 
 
 # Remove oldest build(s) if there's more than MAX_BUILDS builds
-NUM_BUILDS=$(ls ${HOSTNAME}_nightly | wc -l)
-cd ${HOSTNAME}_nightly
+NUM_BUILDS=$(ls | grep ana-nightly | wc -l)
 
 if [ $NUM_BUILDS -gt $MAX_BUILDS ]; then
 	NUM_BUILDS_TO_REMOVE=$(($NUM_BUILDS - $MAX_BUILDS))
-    BUILDS_TO_REMOVE=$(ls -t | tail -n $NUM_BUILDS_TO_REMOVE)
+    BUILDS_TO_REMOVE=$(ls -t | grep ana-nightly | tail -n $NUM_BUILDS_TO_REMOVE)
   
-	echo "$PREFIX Removing $BUILDS_TO_REMOVE build(s)..."
+	echo "$PREFIX Removing $NUM_BUILDS build(s):"
+	echo $BUILDS_TO_REMOVE
 	rm -rf $BUILDS_TO_REMOVE
 else
 	echo "$PREFIX There are less than $MAX_BUILDS builds..."
     echo "$PREFIX No builds to remove..."
 fi
 
-echo "$PREFIX Finished building for $FULL_HOSTNAME as $BUILDER..."
+echo "$PREFIX Finished building for $HOSTNAME as $BUILDER..."
